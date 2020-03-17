273f9f03eaeaa577c2a7ecbcd17d390d3d78ddb3
Solomakhin Anton <s.anton45@yandex.ru>
2020-03-17 14:47:01 +0300

    Tests added

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -13,6 +13,17 @@ class RadioTest {
         radio.setCurrentStation(9);
         radio.increaseCurrentStation();
         assertEquals(0, radio.getCurrentStation());
+        System.out.println("Current Station:" + radio.getCurrentStation());
+    }
+
+    @Test
+    void increaseCurrentStationPositive() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(5);
+        radio.increaseCurrentStation();
+        assertEquals(6, radio.getCurrentStation());
+        System.out.println("Current Station:" + radio.getCurrentStation());
     }
 
     @Test
@@ -22,6 +33,17 @@ class RadioTest {
         radio.setCurrentStation(0);
         radio.decreaseCurrentStation();
         assertEquals(9, radio.getCurrentStation());
+        System.out.println("Current Station:" + radio.getCurrentStation());
+    }
+
+    @Test
+    void decreaseCurrentStationPositive() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(5);
+        radio.decreaseCurrentStation();
+        assertEquals(4, radio.getCurrentStation());
+        System.out.println("Current Station:" + radio.getCurrentStation());
     }
 
     @Test
@@ -31,6 +53,17 @@ class RadioTest {
         radio.setCurrentVolume(10);
         radio.increaseCurrentVolume();
         assertEquals(10, radio.getCurrentVolume());
+        System.out.println("Volume:" + radio.getCurrentVolume());
+    }
+
+    @Test
+    void increaseCurrentVolumePositive() {
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(5);
+        radio.increaseCurrentVolume();
+        assertEquals(6, radio.getCurrentVolume());
+        System.out.println("Volume:" + radio.getCurrentVolume());
     }
 
     @Test
@@ -40,6 +73,17 @@ class RadioTest {
         radio.setCurrentVolume(0);
         radio.decreaseCurrentVolume();
         assertEquals(0, radio.getCurrentVolume());
+        System.out.println("Volume:" + radio.getCurrentVolume());
+    }
+
+    @Test
+    void decreaseCurrentVolumePositive() {
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(5);
+        radio.decreaseCurrentVolume();
+        assertEquals(4, radio.getCurrentVolume());
+        System.out.println("Volume:" + radio.getCurrentVolume());
     }
 
 }
